2d6cbf52ba9cfd94e22be5cefda54a4074bbe095
Boris Aguilar <borisjaguilar@outlook.com>
2024-03-28 19:43:33 +0000

    addded app.js and html code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
+
+//window.onload = function() {
+//write your code here
+//console.log("Hello Rigo from the console!");
+//};
+
+/* eslint-disable */
+let pronouns = ["the", "our"];
+let adjectives = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+let extensions = [".com", ".net", ".us", ".io"]; // Adding extensions
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let pronoun of pronouns) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let extension of extensions) {
+          let domainName = pronoun + adjective + noun + extension;
+          console.log(domainName);
+        }
+      }
+    }
+  }
 };
